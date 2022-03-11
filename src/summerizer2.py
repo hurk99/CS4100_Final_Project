8e1dc7680a858bf43660f6b0445e7214f513fbd8
hurk99 <hur.k@husky.neu.edu>
2022-03-11 15:08:54 -0500

    Reformatted the code

--- src/summerizer2.py
+++ src/summerizer2.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # https://github.com/cristianzsh/youtube-video-maker/blob/master/src/searchrobot.py
-
+# https://github.com/watson-developer-cloud/python-sdk
 import re
 import wikipedia as wiki
 from nltk import tokenize
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
+
 
 class SearchRobot():
 
@@ -15,22 +16,23 @@ class SearchRobot():
         self.sentences_number = 7
 
         self.natural_language_understanding = NaturalLanguageUnderstandingV1(
-                version = "2018-11-16",
-                iam_apikey = "YOUR_API_KEY_HERE",
-                url = "YOUR_URL_HERE")
+            version="2018-11-16",
+            iam_apikey="YOUR_API_KEY_HERE",
+            url="YOUR_URL_HERE")
 
     def search(self, search_term):
-        summary = wiki.summary(search_term, sentences = 7)
+        summary = wiki.summary(search_term, sentences=7)
         summary = re.sub(r"\([^)]*\)", "", summary)
 
         return tokenize.sent_tokenize(summary)
 
     def get_keywords(self, sentences):
         for sentence in sentences:
-            response = self.natural_language_understanding.analyze(text = sentence,
-                    features = Features(
-                        keywords = KeywordsOptions(emotion = True, sentiment = True,
-                            limit = 2))).get_result()
+            response = self.natural_language_understanding.analyze(text=sentence,
+                                                                   features=Features(
+                                                                       keywords=KeywordsOptions(emotion=True,
+                                                                                                sentiment=True,
+                                                                                                limit=2))).get_result()
 
             temp_list = []
             for keyword in response["keywords"]:
